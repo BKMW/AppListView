b5ac7461cb7ba3cb546c21f793abe370356d550d
Refka Benmahmoud <r.benmahmoud@root.sa>
2018-03-05 10:20:26 +0100

    Add project files.

---
+++ AppListView/AppListView/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AppListView
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+            ListView1.ItemsSource = new string[]
+            {
+                "Person1",
+                "Person2",
+                "Person3",
+                "Person4"
+
+            };
+            //ListView1.ItemsSource = new List<Persons>
+            //{
+            //          new Persons{ Name ="person1",Status = "welcome", Image = "http://lorempixel.com/400/200/sports/"},
+            //    new Persons{ Name ="person2",Status = "Hi", Image = "http://lorempixel.com/400/200/sports/"},
+            //    new Persons{ Name ="person3",Status = "Hello", Image = "http://lorempixel.com/400/200/sports/"}
+            //      };
+
+        }
+	}
+}
---
+++ AppListView/AppListView/Persons.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppListView
+{
+    public class Persons
+    {
+        public string Name { get; set; }
+        public string Status { get; set; }
+        public string Image { get; set; }
+    }
+}
